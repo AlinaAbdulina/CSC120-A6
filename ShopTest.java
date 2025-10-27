4e7b737c78cd06c1d8f78e0398292beb96402df3
aabdulina <aabdulina@smith.edu>
2025-10-26 22:46:09 -0400

    in progress

--- ShopTest.java
+++ ShopTest.java
@@ -4,5 +4,83 @@ import org.junit.Test;
 public class ShopTest {
 
     // add your tests here 
-    
+    // setPrice
+    @Test 
+    public void testsetPrice() {
+        Computer myComputer = new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        myComputer.setPrice(1200);
+        assertEquals(1200, myComputer.price);
+
+    }
+
+    // setOS                    
+    @Test
+    public void testsetOS() {
+        Computer myComputer = new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        myComputer.setOS("Windows 10"); 
+        assertEquals("Windows 10", myComputer.operatingSystem); 
+
+
+    }
+
+    // getYear
+    @Test
+    public void testgetYear() {
+        Computer myComputer = new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        int year = myComputer.getYear();
+        assertEquals(2019, year);
+
+    }
+
+    // toString, not sure how to test this one, assertEquals(expected, myComputer.toString())
+    // @Test
+    // public void testtoString() {    
+    //     Computer myComputer = new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+
+    // Resale shop 
+    @Test
+    public void testResaleShop() {
+        ResaleShop shop = new ResaleShop();
+        assertNotNull(shop.inventory);
+        assertEquals(1, shop.inventory.size());
+    }
+
+    // buy
+    @Test
+    public void testbuy() throws Exception {
+        Computer myComputer = new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        ResaleShop shop = new ResaleShop();
+        shop.buy(myComputer); 
+        assertTrue(shop.inventory.contains(myComputer));
+        assertEquals(2, shop.inventory.size());    
+    }
+
+    // sell
+    @Test
+    public void testsell() throws Exception {
+        Computer myComputer = new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        ResaleShop shop = new ResaleShop();
+        shop.sell(myComputer);  
+        assertEquals(0, shop.inventory.size()); 
+    }
+
+    // printInventory, not sure about this one yet.
+    // @Test
+    // public void testprintInventory() {
+    //     Computer myComputer = new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+    //     ResaleShop shop = new ResaleShop();
+    //     shop.printInventory();
+    // }
+
+    // refurbish
+    @Test
+    public void testrefurbish() throws Exception{
+        Computer myComputer = new Computer("2019 MacBook Pro", "Intel", 256, 16, "High Sierra", 2019, 1000);
+        ResaleShop shop = new ResaleShop();
+        shop.refurbish(myComputer, "Windows 10");
+        assertEquals("Windows 10", myComputer.operatingSystem); 
+    }
+
+
+
 }
--- reflection.md
+++ reflection.md
@@ -1,4 +1,6 @@
 Use this file to record your reflection on this assignment.
 
+(Still working on the code)
+
 What are your initial impressions of how `Unit Testing` affects your programming?
 What worked, what didn't, what advice would you give someone taking this course in the future?
